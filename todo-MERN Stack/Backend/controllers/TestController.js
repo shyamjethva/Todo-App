f32b8c95b2d38a35b7c0b69c4153d62a67d421b4
Shyam Jethva <166605762+shyamjethva@users.noreply.github.com>
2025-08-04 12:42:43 +0530

    Delete todo-MERN Stack directory

--- todo-MERN Stack/Backend/controllers/TestController.js
+++
@@ -1,7 +0,0 @@
-const testcontroller = (req, res) => {
-    // res.status(200).send(`<h1>responce from MVC</h1>`);
-    res.send('<h1>responce from MVC</h1>');
-
-}
-
-module.exports = { testcontroller };
